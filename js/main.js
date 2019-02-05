7f7c136ca3ee182cbe4701dfa97f35d99e48d366
Toshiro-Morishita <toshimori1234@gmail.com>
2019-02-05 18:21:54 +0900

    add javascript

---
+++ js/main.js
@@ -0,0 +1,64 @@
+(function() {
+  'use strict';
+
+  let higher = document.getElementById('higher');
+  let lower = document.getElementById('lower');
+  let dealerCard = document.getElementById('dealer_card');
+  let playerCard = document.getElementById('player_card');
+  let wrapper = document.getElementById('wrapper');
+  let result = document.getElementById('result');
+
+  let dealerValue;
+  let playerValue;
+
+  function getRandom() {
+    return Math.floor(Math.random() * 13 + 1);
+  }
+
+  function init() {
+    dealerValue = getRandom();
+    dealerCard.textContent = dealerValue;
+    playerValue = getRandom();
+    playerCard.textContent = playerValue;
+    wrapper.removeEventListener('transitionend', init);
+  }
+
+  function check(guess) {
+    let str;
+    wrapper.classList.add('open');
+    if (playerValue === dealerValue) {
+      str = 'draw'
+    } else {
+      str = 'You ' + getResultStr(guess);
+    }
+    result.textContent = str;
+    result.classList.remove('hidden')
+  }
+
+  function getResultStr(guess) {
+    if (
+      playerValue > dealerValue && guess === 'higher'
+      || playerValue < dealerValue && guess === 'lower'
+    ) {
+      return 'win!';
+    } else {
+      return 'lose...'
+    }
+  }
+
+  init();
+
+  higher.addEventListener('click', function() {
+    check('higher');
+  });
+
+  lower.addEventListener('click', function() {
+    check('lower');
+  });
+
+  dealerCard.addEventListener('click', function() {
+    result.classList.add('hidden');
+    wrapper.classList.remove('open');
+    wrapper.addEventListener('transitionend', init);
+  });
+})();
